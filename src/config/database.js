b3a4d208f0fc4122fe8abdd5a23c23a7fc1362cf
chumaidi <chumaidihxc@gmail.com>
2019-07-16 13:56:41 +0700

    fixing

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
-mongoose.connect(`mongodb://localhost/${process.env.MONGO_DATABASE}`, { useNewUrlParser: true});
 
 const MONGODB_CONNECTION =
-    process.env.MONGODB_ATLAS ||
-    `mongod://localhost/${process.env.MONGO_DATABASE}`;
+  process.env.MONGO_ATLAS ||
+  `mongod://localhost/${process.env.MONGO_DATABASE}`;
 
-    mongosee
-    .connect(MONGODB_CONNECTION,{
-        useNewUrlParser:true
-    })
-
-    .catch(error =>{
-        console.log("error happened when to reach mongodb connection", error);
-    });
+mongoose
+  .connect(MONGODB_CONNECTION, {
+    useNewUrlParser: true
+  })
+  .catch(error => {
+    console.log("error happened when to reach mongodb connection", error);
+  });
 const db = mongoose.connection;
 
 module.exports = db;
